7508c67a3984fa84b1e4ef7b5e40d01c950f3cba
qtvest <113845587+qtvest@users.noreply.github.com>
2022-09-18 11:31:15 -0400

    Create test-install-modules.py

---
+++ Chapter-1/test-install-modules.py
@@ -0,0 +1,9 @@
+
+import yfinance
+import matplotlib.pyplot as plt
+AAPL = yfinance.download(["AAPL"], start="2011-01-04", end="2021-01-04")
+AAPL.head()
+AAPL.tail()
+price = AAPL["Adj Close"]
+price.plot()
+plt.show()
