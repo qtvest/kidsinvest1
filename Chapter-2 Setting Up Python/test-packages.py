336fca94fc5d295a678f209eb97280d3f866f4c1
qtvest <113845587+qtvest@users.noreply.github.com>
2022-10-05 21:16:50 -0400

    Delete Chapter-2 Setting Up Python directory

--- Chapter-2 Setting Up Python/test-packages.py
+++
@@ -1,8 +0,0 @@
-import yfinance
-import matplotlib.pyplot as plt
-AAPL = yfinance.download(["AAPL"], start="2011-01-04", end="2021-01-04")
-AAPL
-price = AAPL["Adj Close"]
-price.plot()
-plt.show()
-
