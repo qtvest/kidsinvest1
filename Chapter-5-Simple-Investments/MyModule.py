031eb420a0c7b72da43fe43a4836893ef884c57d
qtvest <113845587+qtvest@users.noreply.github.com>
2022-10-10 08:25:52 -0400

    Update MyModule.py

--- Chapter-5-Simple-Investments/MyModule.py
+++ Chapter-5-Simple-Investments/MyModule.py
@@ -1,7 +1,3 @@
-import pandas as pd
-import numpy as np
-import random
-
 def single_profit_or_loss(money_spent, buy_price, sell_price):
     shares = money_spent / buy_price
     profit_or_loss = shares * (sell_price - buy_price)
@@ -22,3 +18,20 @@ def equal_split_assets_invest_PnL(all_price_data, asset_tickers, total_money_spe
         total_profit = total_profit + asset_profit        
     return total_profit
     
+def split_assets_invest_PnL(all_price_data, asset_splits, total_money_spent, buy_date, sell_date):
+    sum_of_split = 0
+    for asset,split in asset_splits.items():
+        if split < 0:
+            raise Exception("Asset {} has a negative split {}".format(asset, split))
+        sum_of_split += split
+    if sum_of_split != 1.0:
+        raise Exception("The sum of asset splits must be equal to 1.0")
+        
+    total_profit = 0
+    for asset,split in asset_splits.items():
+        price_data = all_price_data[asset]
+        money_spent_on_asset = total_money_spent * split
+        asset_profit = single_asset_invest_PnL(price_data, money_spent_on_asset, buy_date, sell_date)
+        total_profit = total_profit + asset_profit        
+    return total_profit
+        
